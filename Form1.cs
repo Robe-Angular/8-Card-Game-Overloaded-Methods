fa0553e0b52ea68a3456d42d94ca64bb779beb86
robe <robe@robe.com>
2024-02-05 16:13:35 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _8_Card_Game_Overloaded_Methods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card CardToCheck = new Card((int)Card.Suits.Clubs, (int)Card.Values.Three);
+            bool doesItMatch = Card.DoesCardMatch(CardToCheck, Card.Values.Three);
+            Console.WriteLine(doesItMatch);
+        }
+    }
+}
